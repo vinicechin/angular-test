2dec72177703b1becc7dd6218eb662b3cc80cbe0
Vinicius Cechin <vinicechin@gmail.com>
2018-06-18 15:17:30 -0300

    Added swapi films state controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule} from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+
+import { SwapiReducer } from './store/swapi.reducers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(({ swapi: SwapiReducer}))
   ],
   providers: [],
   bootstrap: [AppComponent]
